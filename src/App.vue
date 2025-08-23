423bec2e9ecc60ae386f98125922e01b01e66031
choichangyun <tuffhrgha704@naver.com>
2025-08-23 14:01:01 +0900

    feat: 프론트엔드 초기 프레임워크

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+  <div class="text-red-500 text-xl font-bold p-4 rounded-lg">
+  Hello Tailwind v4 ✨
+</div>
+</template>
+
+<style scoped></style>
