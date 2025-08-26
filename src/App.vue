75676d456c1cd15bde6cbdb50a5f95d560503b9b
CHOI-074 <tuffhrgha704@naver.com>
2025-08-26 18:22:11 +0900

    fix: 하단 네비게이션 전역으로 수정

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app" class="w-[375px] h-[812px] mx-auto flex flex-col relative overflow-hidden">
     <router-view class="flex-1 overflow-y-auto" />
+    <BottomNav />
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import BottomNav from './components/BottomNav.vue'
+</script>
