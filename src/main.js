69e10e542a00e651af5a67ff4de395c7d0a74f4f
CHOI-074 <tuffhrgha704@naver.com>
2025-08-26 10:06:30 +0900

    chore: Font Awesome 설치

--- src/main.js
+++ src/main.js
@@ -4,10 +4,16 @@ import './assets/main.css'
 
 import App from './App.vue'
 import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faHouse, faUser } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faHouse, faUser)
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')
