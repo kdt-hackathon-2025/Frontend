7041b1f65327a36f75cd0ba7119270d400af4007
wotns <136947201+wotns@users.noreply.github.com>
2025-08-27 20:11:41 +0900

    온보딩 페이지 경로 추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import OnboardingPage from '@/pages/OnboardingPage.vue'
+import OnboardingIntro from '@/pages/OnboardingIntro.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'Onboarding',
+      component: OnboardingPage,
+    },
+    {
+      path: '/intro',
+      name: 'OnboardingIntro',
+      component: OnboardingIntro,
+    },
+  ],
 })
 
 export default router
