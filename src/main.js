327f35aa9954549fbda2a35eddbbf2e59b52fba6
wotns <136947201+wotns@users.noreply.github.com>
2025-08-27 20:10:23 +0900

    글씨체 추가

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import './assets/main.css'
 
 import App from './App.vue'
 import router from './router'
+import 'pretendard/dist/web/static/pretendard.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import {
