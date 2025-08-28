7d55c9e6d9539fcdcf1aac84df0e5249a91b0c59
yellowmodric <kyr7091@naver.com>
2025-08-28 14:22:31 +0900

    feat: 마이페이지 라우터 설정

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ import TestQ4 from '@/pages/test/TestQ4.vue'
 import TestQ5 from '@/pages/test/TestQ5.vue'
 import TestFinish from '@/pages/test/TestFinish.vue'
 import Home from '@/pages/Home.vue'
+import MyPage from '@/pages/MyPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -64,6 +65,11 @@ const router = createRouter({
       path: '/home',
       name: 'Home',
       component: Home,
+    },
+    {
+      path: '/profile',
+      name: 'MyPage',
+      component: MyPage,
     },
   ],
 })
