5a833e20f934c893972d293b37b03b390fc743e4
hwajin <hvvxzin99@gmail.com>
2025-08-29 04:11:42 +0900

    feat: infra 페이지 퍼블리싱 #6

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Home from '@/pages/Home.vue'
 import MyPage from '@/pages/MyPage.vue'
 import Report from '@/pages/Report.vue'
 import InfraPage from '@/pages/Infra/InfraPage.vue'
+import InfraDetail from '@/pages/Infra/InfraDetail.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -78,11 +79,15 @@ const router = createRouter({
       name: 'Report',
       component: Report,
     },
-        {
+    {
       path: '/infra',
       name: 'InfraPage',
       component: InfraPage,
-      meta: { hideBottomNav: true },
+    },
+    {
+      path: '/infraDetail',
+      name: 'InfraDetail',
+      component: InfraDetail,
     },
   ],
 })
